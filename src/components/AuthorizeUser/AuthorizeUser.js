b8db62fe912ebd1f7f89c55b64c095c968b103c4
Tanjil Ahamed <tanjil113355@gmail.com>
2024-11-04 23:24:55 +0600

    new validation added

--- src/components/AuthorizeUser/AuthorizeUser.js
+++ src/components/AuthorizeUser/AuthorizeUser.js
@@ -1,16 +1,17 @@
 "use client";
 
 import { useAddress } from "@thirdweb-dev/react";
-import { useRouter } from "next/navigation";
-import { signIn, useSession, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import Navbar from "../Navbar/Navbar";
 import Contact from "@/page/Home/Contact/Contact";
 import Footer from "../Footer/Footer";
+import Link from "next/link";
+import { IoPersonAddSharp } from "react-icons/io5";
 
 const AuthorizeUser = ({ children }) => {
   const { data: user, status } = useSession();
-  const address = useAddress();
-  const router = useRouter();
+  const wallet = useAddress();
+
   if (status === "loading") {
     return (
       <span className="loading loading-spinner  loading-lg text-white bg-white h-[100vh] mx-auto flex justify-center"></span>
@@ -18,9 +19,34 @@ const AuthorizeUser = ({ children }) => {
   }
 
   return (
-    <div>
+    <div className="bg-black">
       <Navbar />
-      <div className="pt-20 lg:pt-24">{children}</div>
+      <div className="pt-20 lg:pt-24">
+        {!user && (
+          <div className="flex flex-col gap-2 justify-center items-center my-10 h-[30vh] lg:h-[50vh]">
+            <h2 className="text-white text-2xl font-bold">Not Authorized</h2>
+            <Link href={"/"}>
+              {" "}
+              <button className="bg-primary  text-md font-semibold border-none h-10 w-28 hover:bg-[#f2a74b] rounded-lg ">
+                Go to Home
+              </button>
+            </Link>
+          </div>
+        )}
+        {!wallet && user && (
+          <div className="flex flex-col gap-2 justify-center items-center my-10 h-[35vh] lg:h-[50vh]">
+            <IoPersonAddSharp className=" text-4xl text-red-500" />
+            <h2 className="text-red-500 text-2xl font-bold text-center">
+              You are not Register Please Register Yourself
+            </h2>
+            <h2 className="text-white text-base font-bold text-center">
+              If have already registed please reload page after 1-5 minutes
+            </h2>
+          </div>
+        )}
+
+        {user && wallet && children}
+      </div>
       <Contact />
       <Footer />
     </div>
